30dbf61bea0ad1a573c7c44c059dc2799708dd85
Austin Montoya <austin@liminal.sh>
2024-11-11 17:56:37 -0800

    remove netlify directly

--- netlify/prisma/migrations/20241108033315_rename_thumbnail2_cover_image/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `thumbnailUrl` on the `Post` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Post" DROP COLUMN "thumbnailUrl",
-ADD COLUMN     "coverImageUrl" TEXT;
